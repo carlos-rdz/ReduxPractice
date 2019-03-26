78aff79195f4e1244987f0f0b50320ef7bcf4d74
Carlos Rodriguez <carlos.a.rodriguez100@gmail.com>
2019-03-25 21:23:22 -0400

    foundation for our redux practice

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            WE WILL BE PRACTICING REDUX IN THIS REACT APP
           </p>
           <a
             className="App-link"
